270953d48e06821503f0f8ef10ee846552ea1db9
LinardsLiepenieks <linardsliepenieks@gmail.com>
2024-09-03 17:08:59 +0300

    dependencies

--- NLPServer/settings.py
+++ NLPServer/settings.py
@@ -46,7 +46,7 @@ LOGGING = {
         # Add other loggers as needed
     },
 }
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
